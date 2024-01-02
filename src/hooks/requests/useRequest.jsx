245a3db0c1cafb9235cbf011d0bc417490e4e237
Daniella-Rocha <daniella.fp.dr@gmail.com>
2024-01-02 16:01:51 -0300

    Criada função editar, excluir, adicionar novo item

--- src/hooks/requests/useRequest.jsx
+++ src/hooks/requests/useRequest.jsx
@@ -8,13 +8,14 @@ export const useRequestItems = () => {
     const [categories, setCategories] = useState([]);
 
     const requestAllItems = async () => {
+
+        // requisição para trazer todos os itens da despensa
+
         try {
-            const response = await requestPantryOrganizer.get('/all-items');
+            const response = await requestPantryOrganizer.get('/v1/all-items');
 
             const data = await response.data;
-            
-            console.log(data);
-            
+
             setItemsList(data);
 
         } catch (error) {
@@ -25,8 +26,10 @@ export const useRequestItems = () => {
 
     const requestAllCategories = async () => {
 
+        // requisição para trazer todas as categorias da despensa
+
         try {
-            const reponse = await requestPantryOrganizer.get('/all-categories');
+            const reponse = await requestPantryOrganizer.get('/v1/all-categories');
 
             const data = await reponse.data;
 
@@ -38,21 +41,74 @@ export const useRequestItems = () => {
     }
 
     const updateItem = async (id, item) => {
+
+        // requisição para atualizar um item via axios
+
         try {
-            const response = await requestPantryOrganizerMockable.patch(`${id}`,
+            const response = await requestPantryOrganizer.patch(`/v1/all-items/${id}`,
                 {
                     method: "PATCH",
                     body: item
                 }
             );
 
-            const updated = await response.data;
+            console.log(item);
+            
+            const newList = itemsList.filter(item => item.id !== id);
 
-            console.log(updated);
+            setItemsList(() => {
+                return [
+                    ...newList,
+                    {...item}
+                ]
+            });
 
         } catch (error) {
+            console.log("Error fetching item:", error);
+        }
+    }
+
+    const createNewItem = async (item) => {
+
+        // requisição para adicionar um novo item via axios
+
+        try {
+            const response = await requestPantryOrganizer.post('/v1/all-items',
+
+                {
+                    method: "POST",
+                    body: item
+                }
+            )
+
+            // const dataPost = response.data;
+
             console.log(item);
-            console.error("Error fetching item:", error);
+
+        } catch (error) {
+            console.log("Error fetching item:", error);
+        }
+    }
+    const deleteAnItem = async (id, item) => {
+
+        // requisição para excluir um item via axios
+
+        try {
+            const response = await requestPantryOrganizer.delete(`/v1/all-items/${id}`,
+                {
+                    method: "DELETE",
+                    body: item
+                }
+            )
+
+            const dataDeleted = itemsList.filter(item => item.id !== id);
+
+            setItemsList(dataDeleted);
+
+            console.log(item);
+
+        } catch (error) {
+            console.log("Error deleting item:", error);
         }
     }
 
@@ -63,11 +119,12 @@ export const useRequestItems = () => {
     useEffect(() => {
         requestAllCategories();
     }, []);
-
-
+    
     return {
         itemsList,
         categories,
-        updateItem
+        updateItem,
+        createNewItem,
+        deleteAnItem
     }
 }
