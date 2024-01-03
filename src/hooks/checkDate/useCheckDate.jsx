6c34464ec5edf8781c1957bd596fa81b8162ab8d
Daniella-Rocha <daniella.fp.dr@gmail.com>
2024-01-03 18:39:37 -0300

    criado hook para verificar se o produto vai expirar em breve

--- src/hooks/checkDate/useCheckDate.jsx
+++ src/hooks/checkDate/useCheckDate.jsx
@@ -3,21 +3,33 @@ import { useEffect, useState } from "react";
 import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br';
 
-export const useExpiresSoon = (date) => {
-    const [expiresSoon, setExpiresSoon] = useState(false);
-    
-    const setDate = 7;
-    const dateProduct = dayjs(date);
-    const diference = dateProduct.diff(dayjs(), 'day') + 1;
-    const checkDate = diference <= setDate;
-    
-    useEffect(() =>{
-        setExpiresSoon(checkDate);
-    }, []);
-    
-    return{
-        checkDate,
-        expiresSoon,
-        diference
+export const useExpiresSoon = () => {
+
+    // const setDate = 7;
+    // const dateProduct = dayjs(date);
+    // const diference = dateProduct.diff(dayjs(), 'day') + 1;
+    // const checkDate = diference <= setDate;
+
+    const checkExpiresSoon = (date) => {
+        const setDate = 7;
+        const dateProduct = dayjs(date);
+        const diference = dateProduct.diff(dayjs(), 'day') + 1;
+        const checkDate = diference <= setDate;
+
+        return checkDate;
+    }
+
+    const checkDiference = (date) => {
+        const dateProduct = dayjs(date);
+        const diference = dateProduct.diff(dayjs(), 'day') + 1;
+
+        return diference;
+    }
+
+    return {
+        // checkDate,
+        // diference,
+        checkDiference,
+        checkExpiresSoon
     }
 }
