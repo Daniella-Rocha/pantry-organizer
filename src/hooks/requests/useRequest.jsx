3dade183da3e420ea6361c2ec9401be65e8fd196
Daniella-Rocha <daniella.fp.dr@gmail.com>
2024-01-03 18:41:02 -0300

    criadas as chamadas de api para deletar, excluir e editar um item ou categoria

--- src/hooks/requests/useRequest.jsx
+++ src/hooks/requests/useRequest.jsx
@@ -42,24 +42,23 @@ export const useRequestItems = () => {
 
     const updateItem = async (id, item) => {
 
+        const newItem = JSON.stringify(item);
         // requisição para atualizar um item via axios
 
         try {
             const response = await requestPantryOrganizer.patch(`/v1/all-items/${id}`,
                 {
                     method: "PATCH",
-                    body: item
+                    body: newItem
                 }
             );
 
-            console.log(item);
-            
-            const newList = itemsList.filter(item => item.id !== id);
+            console.log(newItem);
 
-            setItemsList(() => {
+            setItemsList((prevState) => {
                 return [
-                    ...newList,
-                    {...item}
+                    ...prevState,
+                    { ...newItem }
                 ]
             });
 
@@ -81,16 +80,22 @@ export const useRequestItems = () => {
                 }
             )
 
-            // const dataPost = response.data;
+            const dataPost = response.data.message;
 
             console.log(item);
+
+            setItemsList(prevState =>
+                [
+                    ...prevState,
+                    { ...item }
+                ]
+            )
 
         } catch (error) {
             console.log("Error fetching item:", error);
         }
     }
     const deleteAnItem = async (id, item) => {
-
         // requisição para excluir um item via axios
 
         try {
@@ -105,10 +110,83 @@ export const useRequestItems = () => {
 
             setItemsList(dataDeleted);
 
-            console.log(item);
-
         } catch (error) {
             console.log("Error deleting item:", error);
+        }
+    }
+
+    // requisição para criar nova cetgoria via axios
+    const newCategory = async (item) => {
+
+        const newItem = JSON.stringify(item);
+
+        try {
+            const response = await requestPantryOrganizer.post('/v1/all-categories',
+                {
+                    method: "POST",
+                    body: newItem
+                }
+            );
+
+            console.log(response.data.message);
+
+            console.log(item);
+
+            setCategories(prevState =>
+                [
+                    ...prevState,
+                    { ...item }
+                ]
+            );
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    // deletar uma categoria
+    const deleteACategory = async (item, id) => {
+
+        try {
+            const response = await requestPantryOrganizer.delete(`/v1/all-categories/${item.id}`,
+                {
+                    method: "DELETE",
+                    body: item
+                }
+            )
+
+            console.log(response.data.message);
+
+            // rotarna as categorias com id diferente, causando atualização visual da exclusão de categorias
+            const updatedList = categories.filter(item => item.id !== id);
+
+            // atualiza a lista de categorias c
+            setCategories(updatedList);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const updateCategory = async (id, item) => {
+        try {
+            const response = await requestPantryOrganizer.patch(`/v1/all-categories/${id}`,
+                {
+                    method: "PATCH",
+                    body: item
+                }
+            )
+
+            console.log(response.data.message);
+
+            setCategories(prevState =>
+                [
+                    ...prevState
+                ]
+            )
+
+        } catch (error) {
+            console.log(error);
         }
     }
 
@@ -119,12 +197,19 @@ export const useRequestItems = () => {
     useEffect(() => {
         requestAllCategories();
     }, []);
-    
+
+    useEffect(() => {
+        setItemsList(itemsList);
+    }, [itemsList]);
+
     return {
         itemsList,
         categories,
         updateItem,
         createNewItem,
-        deleteAnItem
+        deleteAnItem,
+        newCategory,
+        deleteACategory,
+        updateCategory
     }
 }
